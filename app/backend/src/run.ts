719228258e65e7e35b864530dae4cd821a2b019d
Michael Belousov <michael.belousov98@gmail.com>
2023-10-18 00:12:56 -0400

    add env var check

--- app/backend/src/run.ts
+++ app/backend/src/run.ts
@@ -3,4 +3,13 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
+// FIXME: generate from .env template
+const requiredEnvVars = [
+  "GITHUB_CLIENT_ID"
+];
+
+for (const _var of requiredEnvVars)
+  if (process.env[_var] === undefined)
+    throw Error(`required env var '${_var}' is missing!`);
+
 void run();

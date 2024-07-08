53cf2cd9554362384178e526bcf2740b4599400e
Michael Belousov <michael.belousov98@gmail.com>
2024-07-08 14:11:46 +0200

    delete non-local backend

--- provisioning/test_deployment.sh
+++
@@ -1,6 +0,0 @@
-# FIXME: there probably exists a way to run a full script from local path
-ssh mike@alternis.io '
-  cd ~/alternis-v1-deploy-stage/DialogueMiddleware-staging/app/backend \
-  && pnpm exec prisma generate \
-  && PORT=3000 nohup bun lib/run.js'
-# FIXME: use tmux instead for test deployments

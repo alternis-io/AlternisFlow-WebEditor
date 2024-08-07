c35f795ce1930c0884dbdc795d240842797678ca
Michael Belousov <michael.belousov98@gmail.com>
2024-08-07 06:28:52 -0400

    fix up about dialog

--- app/frontend/components/Header.tsx
+++ app/frontend/components/Header.tsx
@@ -2,23 +2,87 @@ import React from "react";
 import { Link, useLocation, useNavigate } from "react-router-dom";
 import styles from "./Header.module.css";
 import { Split } from "../Split";
-import { resetAllAppState, useAppState } from "../AppState";
-import downloadFile, { uploadFile } from "../localFileManip";
-import { useApi } from "../api/useApi";
 import { baseUrl } from "../api/useRemoteApi";
 import logoPath from "../../../resources/logo2.svg";
-import { exportCurrentDialogueToJson } from "../export";
 import { useTutorialStore } from "../Tutorial1";
 
+// FIXME: reuse
+export function useOnExternalClick<T extends HTMLElement | null, U extends HTMLElement | null>(
+  callback: () => void,
+  args: {
+    ref: React.MutableRefObject<T>,
+    trigger: React.MutableRefObject<U> | undefined,
+  },
+): void {
+  const { ref, trigger } = args;
+  React.useEffect(() => {
+    function handleClickOutside(e: any) {
+      if (ref.current === null)
+        return;
+      if (trigger !== undefined && trigger.current === null)
+        return;
+
+      const outsideButNotInTrigger
+        = trigger !== undefined
+        ? !ref.current.contains(e.target) && !trigger.current!.contains(e.target)
+        : !ref.current.contains(e.target);
+
+      if (outsideButNotInTrigger) {
+        callback();
+      }
+    }
+
+    // Bind the event listener
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      // Unbind the event listener on clean up
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [trigger, ref, callback]);
+}
+
 export function Header() {
-  const logout = useApi(s => s.auth?.logout);
   const location = useLocation();
 
+  // FIXME: put this in the hash
   // FIXME: do real query param parsing!
-  const noHeaderRequested = location.search.includes("noHeaderLogo");
+  const noHeaderRequested
+    = location.search.includes("noHeaderLogo")
+    || location.hash.includes("noHeaderLogo");
 
   const navigate = useNavigate();
   const [aboutModalOpen, setAboutModalOpen] = React.useState(false);
+
+  const dialogRef = React.useRef<HTMLDialogElement>(null);
+  const aboutButtonRef = React.useRef<HTMLElement>(null);
+
+  // FIXME: this doens't work when pressing the grid which is important
+  useOnExternalClick(() => {
+      setAboutModalOpen(false);
+    }, {
+    ref: dialogRef,
+    trigger: aboutButtonRef,
+  });
+
+
+  const aboutDialog = (
+    <dialog className={styles.aboutDialog} open={aboutModalOpen} ref={dialogRef}>
+      <h2> The Alternis Editor </h2>
+      <p>
+        A visual editor for the Alternis dialogue middleware's dialogue format.
+      </p>
+      <p>
+        Open source at <a href="https://github.com/alternis-io/DialogueMiddleware">Github</a>.
+      </p>
+      <p>
+        Check out the tutorial in the header for help but feel free to use the feedback link or contact
+        the author.
+      </p>
+      <p>
+        Made by <a href="https://mikemikeb.com">Michael Belousov</a>
+      </p>
+    </dialog>
+  );
 
   return !noHeaderRequested && <Split
     style={{
@@ -74,16 +138,9 @@ export function Header() {
           Feedback
         </nav>
         {/* FIXME: add dialog */}
-        <nav className="alternis__hoverable" onClick={() => setAboutModalOpen(p => !p)}>
+        <nav ref={aboutButtonRef} className="alternis__hoverable" onClick={() => setAboutModalOpen(p => !p)}>
           About
-          <dialog className={styles.aboutDialog} open={aboutModalOpen}>
-            <p>
-              <a href="">Github</a>
-            </p>
-            <p>
-              by <a href="https://mikemikeb.com">Michael Belousov</a>
-            </p>
-          </dialog>
+          {aboutDialog}
         </nav>
       </div>
     }

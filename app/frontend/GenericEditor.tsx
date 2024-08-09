354dce33f1a8d7f55f4229f9826c898cde8dc9d3
Michael Belousov <michael.belousov98@gmail.com>
2024-08-09 14:47:53 -0400

    Revert "attempt remove react warning for project data editor"
    This reverts commit d18458ccecb3a81482e96c3759ed8a8d24c03da1.

--- app/frontend/GenericEditor.tsx
+++ app/frontend/GenericEditor.tsx
@@ -104,11 +104,6 @@ export function GenericEditor<T extends SupportedKeys>(
 
   const ExtraActions = props.extraActions ?? React.useRef((_p: any) => null).current;
 
-  React.useLayoutEffect(() => {
-    if (proposedNameInputRef.current && proposedName !== undefined)
-      proposedNameInputRef.current.textContent = proposedName;
-  }, [proposedName]);
-
   return (
     <div
       {...divProps}
@@ -193,7 +188,9 @@ export function GenericEditor<T extends SupportedKeys>(
                 props.onRename?.(keyBeingEdited, value);
               }
             }}
-          />
+          >
+            {proposedName}
+          </div>
         </div>
       : <div
           data-tut-id="generic-proj-data-add-button"
